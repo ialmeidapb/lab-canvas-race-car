9e70c734f7407e59ead4779113a0491cfaafb55e
ialmeida <ialmeida.pb@gmail.com>
2021-04-02 16:36:01 -0300

    done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,251 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+
+
+const crashSound = new Audio()
+crashSound.src= `./sounds/campanhia.mp3`
+crashSound.volume = 0.1;
+
+class GameObject {
+  constructor(x, y, width, height, img) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+    this.speedX = 0;
+    this.speedY = 0;
+  }
+
+  updatePosition() {
+    this.x += this.speedX;
+
+    if (this.x <= 40) {   //limites da pista
+      this.x = 40;
+    }
+
+    if (this.x >= canvas.width-90) {
+      this.x = canvas.width - 90;
+    }
+
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
+  crashWith(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
 
 
 
+
+}
+
+class BackgroundImage extends GameObject {
+  constructor(x, y, width, height, img) {
+    super(x, y, width, height, img);
+    this.speedY = 3;
+  }
+
+  updatePosition() {
+    this.y += this.speedY;
+    this.y %= canvas.height;
+  }
+
+  draw() {
+    ctx.drawImage(this.img, 0, this.y, this.width, this.height);
+    ctx.drawImage(this.img, 0, this.y - canvas.height, this.width, this.height);
+  }
+}
+
+class Obstacle extends GameObject{
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.speedY = 3
+  }
+
+ draw() {
+   ctx.fillStyle = 'blue';
+   ctx.fillRect(this.x, this.y, this.width, this.height);
+ }
+
+
+}
+
+
+
+class Game {
+  constructor(background, player) {
+    this.background = background;
+    this.player = player;
+    this.obstacles = [];
+    this.frames = 0;
+    this.score = 0;
+    this.animationId;
+  }
+
+  start = () => {
+    this.updateGame();
+  };
+
+  updateGame = () => {
+    this.clear();
+
+    this.background.updatePosition();
+    this.background.draw();
+
+    this.player.updatePosition();
+    this.player.draw();
+
+    this.updateObstacles();
+
+    this.updateScore ()
+
+    this.animationId = requestAnimationFrame(this.updateGame);
+
+    this.checkGameOver();
+  };
+
+
+  updateObstacles = ()=>{
+    this.frames++;
+  
+    for(let i=0; i< this.obstacles.length; i++){
+      this.obstacles[i].updatePosition();
+      this.obstacles[i].draw();
+    }
+    
+
+    if(this.frames%120 === 0){
+    const originY = 0;
+
+
+    const minX=50;
+    const maxX=240;
+    const randomX = Math.floor(Math.random() * (maxX - minX +1))+minX;
+
+    const minWidth=50;
+    const maxWidth=240;
+    const randomWidth = Math.floor(Math.random() * (maxWidth - minWidth +1))+minWidth;
+
+    const obstacle = new Obstacle(randomX, originY, randomWidth, 20);
+
+    this.obstacles.push(obstacle);
+    
+    this.score++;
+  
+  }
+
+    
+
+  };
+
+
+  checkGameOver = () =>{
+    const crashed = this.obstacles.some((obstacle) =>{
+      return this.player.crashWith(obstacle);
+
+    });
+
+    if (crashed) {
+      crashSound.play();
+
+      cancelAnimationFrame(this.animationId);
+    
+      this.gameOver();
+
+      
+    }
+  }
+
+
+  gameOver(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+
+    ctx.fillStyle = "red";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Game Over!", canvas.width / 2, 200);
+
+    ctx.font = "30px Verdana";
+    ctx.fillStyle = "white";
+    ctx.fillText(`Your Final Score: ${this.score}`, canvas.width / 6, 400);
+
+
+  }
+
+  clear = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+
+  updateScore(){
+    ctx.font = '30px Verdana'
+    ctx.fillStyle = "white";
+    ctx.fillText(`Score: ${this.score}`, 100, 40);
+  }
+
+
+}
+
+function startGame() {
+  // Instanciando todas as imagens
+  const bgImg = new Image();
+  bgImg.src = "./images/road.png";
+
+  const carImg = new Image();
+  carImg.src = "./images/car.png";
+
+  const backgroundImage = new BackgroundImage(
+    0,
+    0,
+    canvas.width,
+    canvas.height,
+    bgImg
+  );
+  const player = new GameObject(250 - 25, canvas.height - 120, 50, 100, carImg);
+
+  const game = new Game(backgroundImage, player);
+
+  game.start();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "ArrowLeft") {
+      game.player.speedX = -3;
+    } else if (event.code === "ArrowRight") {
+      game.player.speedX = 3;
+    }
+  });
+
+  document.addEventListener("keyup", () => {
+    game.player.speedX = 0;
+  });
+}
 
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
-  function startGame() {
-    const img = new Image();
-    const car = new Image();
-    car.src = `./images/car.png`;
-    img.src = `./images/road.png`;
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.drawImage(car, 225, 620, 50, 80);
-    };
 };
-}
